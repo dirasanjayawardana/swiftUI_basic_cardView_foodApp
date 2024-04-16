a4e5461f404780358ce376d1403b073081e5276f
Mona Lisa <andreno.juanda11@gmail.com>
2024-04-16 10:50:04 +0700

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by MACBOOK PRO on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
